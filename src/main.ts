56d690b3956f3eb89af68c27a956a55486f61a12
ishola10 <akoredeishola10@gmail.com>
2025-02-03 18:17:22 +0100

    ft-pixel-tracker

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,30 @@
-import './assets/main.css'
+import "./assets/main.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-const app = createApp(App)
+const firebaseConfig = {
+  apiKey: "AIzaSyDTLfyBoWJ5oyVq41VW8Hqfu16U0FL_st8",
+  authDomain: "testing-f1970.firebaseapp.com",
+  projectId: "testing-f1970",
+  storageBucket: "testing-f1970.firebasestorage.app",
+  messagingSenderId: "999659174530",
+  appId: "1:999659174530:web:c56e6842eb4866eb2f129b",
+  measurementId: "G-2J1CJFSH2S",
+};
 
-app.use(router)
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+
+const app = createApp(App);
+app.use(router);
 
 AOS.init();
 
-app.mount('#app')
+app.mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,22 @@ import Gallery from '@/views/Gallery.vue'
 import Project from '@/views/Project.vue'
 // import AddProject from '@/views/AddProject.vue'
 
+import { getAnalytics, logEvent } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTLfyBoWJ5oyVq41VW8Hqfu16U0FL_st8",
+  authDomain: "testing-f1970.firebaseapp.com",
+  projectId: "testing-f1970",
+  storageBucket: "testing-f1970.firebasestorage.app",
+  messagingSenderId: "999659174530",
+  appId: "1:999659174530:web:c56e6842eb4866eb2f129b",
+  measurementId: "G-2J1CJFSH2S"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -44,6 +60,10 @@ const router = createRouter({
       component: () => import('../views/NotFound.vue')
     }
   ]
-})
+});
+
+router.afterEach((to) => {
+  logEvent(analytics, 'page_view', { page_path: to.fullPath });
+});
 
 export default router
